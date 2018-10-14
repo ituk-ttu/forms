5d10978b281c59ebd0d45b238c12f1ec65dee01d
Kaur <kaur.palang@hotmail.com>
2018-10-14 19:13:35 +0300

    JSON response for '/submit'

--- src/main/java/ee/ituk/controllers/WebController.java
+++ src/main/java/ee/ituk/controllers/WebController.java
@@ -2,6 +2,7 @@ package ee.ituk.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import ee.ituk.database.ResponseModel;
 import ee.ituk.database.ResponseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +27,19 @@ public class WebController {
     @PostMapping("/submit")
     @ResponseBody
     public String submit(@RequestBody String responseString) {
+        ObjectNode root = factory.objectNode();
         try {
             ResponseModel responseModel = mapper.readValue(responseString, ResponseModel.class);
             responseRepo.save(responseModel);
-            return "yep";
+
+            root.put("status", "OK");
+            root.put("info", "Response saved");
+            return root.toString();
         } catch (Exception e) {
             e.printStackTrace();
-            return "Nope";
+            root.put("status", "NOT OK");
+            root.put("error", e.getClass().getSimpleName());
+            return root.toString();
         }
     }
 }
